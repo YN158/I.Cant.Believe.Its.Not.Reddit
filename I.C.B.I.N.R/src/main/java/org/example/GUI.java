cbcb393839ecbe43bfcbd4a6d389e7dc18e5d66f
YasharNasiri <bluemustarduniversity@gmail.com>
2024-04-03 17:09:15 +0330

    Set up the GUI

---
+++ I.C.B.I.N.R/src/main/java/org/example/GUI.java
@@ -0,0 +1,34 @@
+package org.example;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.event.WindowEvent;
+
+public class GUI
+{
+    JFrame frame;
+    JButton button;
+    JButton button2;
+
+    public GUI(String name)
+    {
+        frame = new JFrame(name);
+
+        button = new JButton();
+        button.setBounds(200,100, 100, 50);
+        button2 = new JButton();
+        button2.setBounds(200,400, 100, 50);
+        button2.setVisible(false);
+
+
+        button.addActionListener(e -> {button2.setVisible(true); button.setVisible(false);});
+        button2.addActionListener(e -> {frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING)); System.out.println("poo");});
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(500, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.add(button);
+        frame.add(button2);
+    }
+}
--- I.C.B.I.N.R/src/main/java/org/example/Main.java
+++ I.C.B.I.N.R/src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        GUI gui = new GUI("I.C.B.I.N.R");
     }
 }
